53f9c17e522744155568ab7a2b699212c645b690
Ted Ciafardini - personal <esciafardini@gmail.com>
2025-07-23 12:45:54 -0400

    First pass and simplified personal nvim

---
+++ lua/vim-options.lua
@@ -0,0 +1,8 @@
+-- generic vim settings
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+-- Conjure Override K setting
+vim.g["conjure#mapping#doc_word"] = "gk"
