24c818132fecea322253bfb2d99439a4042af544
esciafardini <edward.ciafardini@aclaimant.com>
2026-01-02 11:29:38 -0500

    New formatting keymaps
    (form specific vs apply to file)

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -11,7 +11,29 @@ return {
       vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Go To Reference" })
       vim.keymap.set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, { desc = "Code Actions" })
       vim.keymap.set({ "n" }, "<leader>lr", vim.lsp.buf.rename, { desc = "Rename" })
-      vim.keymap.set({ "n" }, "<leader>lf", vim.lsp.buf.format, { desc = "Format File" })
+      vim.keymap.set({ "n" }, "<leader>lF", vim.lsp.buf.format, { desc = "Format File" })
+      vim.keymap.set("n", "<leader>lf", function()
+        local node = vim.treesitter.get_node()
+        -- Clojure form types in treesitter
+        local form_types = { "list_lit", "vec_lit", "map_lit", "set_lit", "anon_fn_lit" }
+        while node do
+          local node_type = node:type()
+          for _, form_type in ipairs(form_types) do
+            if node_type == form_type then
+              local start_row, start_col, end_row, end_col = node:range()
+              vim.lsp.buf.format({
+                range = {
+                  start = { start_row + 1, start_col },
+                  ["end"] = { end_row + 1, end_col },
+                }
+              })
+              return
+            end
+          end
+          node = node:parent()
+        end
+        print("No form found")
+      end, { desc = "Format form" })
 
       vim.lsp.enable('clojure_lsp')
     end,
