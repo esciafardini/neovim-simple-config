6d3f30a6853de7224ac3b14b35c34f0b2e6caea9
esciafardini <edward.ciafardini@aclaimant.com>
2026-01-13 21:41:45 -0500

    alphabetizing
    i think i'm done now...

--- lua/options.lua
+++ lua/options.lua
@@ -37,17 +37,18 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 -- Keymaps
-vim.keymap.del("n", "gc")
+vim.keymap.set("i", "jkj", "<Esc>", { noremap = false })
 vim.keymap.set("v", "J", ":move '>+1<CR>gv-gv", { desc = "Move block downwards" })
 vim.keymap.set("v", "K", ":move '<-2<CR>gv-gv", { desc = "Move block upwards" })
-vim.keymap.set("n", "<C-f>", "<C-d>", { desc = "Halfscroll down" })
 vim.keymap.set("n", "<C-b>", "<C-u>", { desc = "Halfscroll up" })
-vim.keymap.set("n", "<leader>h", ":nohl<CR>", { desc = "Unhighlight" })
-vim.keymap.set("i", "jkj", "<Esc>", { noremap = false })
-vim.keymap.set("n", "<leader>ar", "<cmd>Telescope smart_open<cr>", { desc = "Recent files" })
+vim.keymap.set("n", "<C-f>", "<C-d>", { desc = "Halfscroll down" })
 vim.keymap.set("n", "<leader>ac", function()
+vim.keymap.set("n", "<leader>ar", "<cmd>Telescope smart_open<cr>", { desc = "Recent files" })
+vim.keymap.set("n", "<leader>h", ":nohl<CR>", { desc = "Unhighlight" })
   vim.cmd("Neotree position=float dir=" .. vim.fn.stdpath("config"))
 end, { desc = "Nvim Config" })
+-- delete gc
+vim.keymap.del("n", "gc")
 
 -- Start screen keymap
 vim.api.nvim_create_autocmd("VimEnter", {
