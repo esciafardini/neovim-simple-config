6a2c872fc9fc0c6c9caa45ce2c472db7de73e69f
Ted Ciafardini - personal <esciafardini@gmail.com>
2026-01-19 00:25:04 -0500

    Get rid of "bigfile"
    LunarVim stuff seems to have become abandonware and I am getting
    "deprecated vim fn" errors for a plugin that I don't even use.

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -1,12 +1,5 @@
 return {
-  {
-    "LunarVim/bigfile.nvim",
-    event = "BufReadPre",
-    opts = {},
-  },
-  {
-    "karb94/neoscroll.nvim",
-    event = "VeryLazy",
-    opts = {},
-  },
+  "karb94/neoscroll.nvim",
+  event = "VeryLazy",
+  opts = {},
 }
