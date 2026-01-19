6278f78a564279aaad52d6fc66462e469cb487ec
Ted Ciafardini - personal <esciafardini@gmail.com>
2026-01-18 23:37:39 -0500

    Use gitsigns instead of Vim Fugitive
    I just...
    don't like vim fugitive
    
    This provides are nice UI with the ability to undo changes.
    
    Add gitsigns
    
    Added gitsigns with some cool lua to help out with staging/resetting
    
    This has a pretty incredible Git Blame functionality, so no more
    Fugitive (yess)

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,3 +1,20 @@
+local function hunk_nav()
+  vim.notify("Nav'ing to next hunk")
+  vim.cmd("Gitsigns next_hunk")
+end
+
+local function select_to_stage()
+  vim.cmd("Gitsigns select_hunk")
+  vim.cmd("Gitsigns stage_hunk")
+  vim.notify("Hunk has been staged ✓")
+end
+
+local function select_to_reset()
+  vim.cmd("Gitsigns select_hunk")
+  vim.cmd("Gitsigns stage_hunk")
+  vim.notify("Hunk has been reset ←")
+end
+
 return {
   {
     "kdheepak/lazygit.nvim",
@@ -17,9 +34,14 @@ return {
     }
   },
   {
-    "tpope/vim-fugitive",
+    "lewis6991/gitsigns.nvim",
     keys = {
-      { "<leader>gb", "<cmd>Git blame<cr>", desc = "Git Blame" }
+      { "<leader>gb", "<cmd>Gitsigns toggle_current_line_blame<cr>", desc = "Git Blame Line" },
+      { "<leader>gB", "<cmd>Gitsigns blame<cr>",                     desc = "Git Blame Toggle" },
+      { "<leader>gp", "<cmd>Gitsigns preview_hunk_inline<cr>",       desc = "Git Preview" },
+      { "<leader>gn", hunk_nav,                                      desc = "Git Nav To Next Hunk" },
+      { "<leader>gs", select_to_stage,                               desc = "Git Stage Hunk" },
+      { "<leader>gr", select_to_reset,                               desc = "Git Reset Hunk" },
     },
   },
 }
