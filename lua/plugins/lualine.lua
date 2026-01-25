281382b515c4253d9aa6d7514012b8a6c85c2135
esciafardini <edward.ciafardini@aclaimant.com>
2026-01-24 20:59:02 -0500

    Use opts when possible
    Reading LazyGit docs and following orders

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,43 +1,41 @@
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" }, -- for "cool" file symbols
-  config = function()
-    require('lualine').setup({
-      options = {
-        theme = 'auto'
-      },
-      tabline = {
-        lualine_a = {
-          {
-            'buffers',
-            fmt = function(name, context)
-              local bufnr = context.bufnr
-              local bufname = vim.api.nvim_buf_get_name(bufnr)
-              -- Respect :file renames for terminal buffers
-              if vim.bo[bufnr].buftype == 'terminal' and not bufname:match('^term://') then
-                return vim.fn.fnamemodify(bufname, ':t')
-              end
-              return name
+  opts = {
+    options = {
+      theme = 'auto'
+    },
+    tabline = {
+      lualine_a = {
+        {
+          'buffers',
+          fmt = function(name, context)
+            local bufnr = context.bufnr
+            local bufname = vim.api.nvim_buf_get_name(bufnr)
+            -- Respect :file renames for terminal buffers
+            if vim.bo[bufnr].buftype == 'terminal' and not bufname:match('^term://') then
+              return vim.fn.fnamemodify(bufname, ':t')
             end
-          }
+            return name
+          end
+        }
+      },
+    },
+    sections = {
+      lualine_c = {
+        'filename',
+        { function() return ' 🌜 ' end, padding = { left = 1, right = 0 } },
+        {
+          function()
+            return vim.bo.filetype:match('gitsigns') and 'BLAME' or ''
+          end,
+          color = { fg = '#ff9e64' },
         },
       },
-      sections = {
-        lualine_c = {
-          'filename',
-          { function() return ' 🌜 ' end, padding = { left = 1, right = 0 } },
-          {
-            function()
-              return vim.bo.filetype:match('gitsigns') and 'BLAME' or ''
-            end,
-            color = { fg = '#ff9e64' },
-          },
-        },
-        lualine_x = {
-          { 'filetype',  icon_only = true },
-          { 'lsp_status' }
-        },
+      lualine_x = {
+        { 'filetype',  icon_only = true },
+        { 'lsp_status' }
       },
-    })
-  end,
+    },
+  }
 }
--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -21,25 +21,23 @@ return {
     { "<leader>e", function() open_neotree() end, desc = "File Explorer" },
     { "<leader>ac", function() open_neotree(vim.fn.stdpath("config")) end, desc = "Nvim Config" },
   },
-  config = function()
-    require("neo-tree").setup({
-      default_component_configs = {
-        git_status = { enabled = false },
-        diagnostics = { enabled = false },
-      },
-      git_status_async = false,
-      enable_git_status = false,
-      filesystem = {
-        filtered_items = {
-          visible = true,
-          hide_dotfiles = false,
-          hide_gitignored = false,
-          never_show = {
-            ".idea", "target", "tmprelease", ".git", ".DS_Store", ".lein-failures",
-            ".lein-repl-history", "acl.iml", "figwheel_server.log", "node_modules",
-          },
+  opts = {
+    default_component_configs = {
+      git_status = { enabled = false },
+      diagnostics = { enabled = false },
+    },
+    git_status_async = false,
+    enable_git_status = false,
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        never_show = {
+          ".idea", "target", "tmprelease", ".git", ".DS_Store", ".lein-failures",
+          ".lein-repl-history", "acl.iml", "figwheel_server.log", "node_modules",
         },
       },
-    })
-  end,
+    },
+  }
 }
