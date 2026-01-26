7a824746890f6a5f8c25c1316233d2f55d00a72b
esciafardini <edward.ciafardini@aclaimant.com>
2026-01-25 20:09:14 -0500

    Folding whichkey niceties

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -9,9 +9,8 @@ return {
       "zH", "zL", "zs", "ze",       -- horizontal scroll
       "zg", "zw", "z=",             -- spell
       "zx", "zd", "zD", "zf", "zE", -- delete/create folds
-      "zr", "zR",                   -- fold less
-      "zm", "zM",                   -- fold more
-      "zb", "zt", "zv", "z<cr>"     -- w/e
+      "zb", "zt", "zv", "z<CR>",    -- b.s.
+      "zo", "zO", "zc", "zC",       -- w/e, just use za, zA
     }
     local hidden_specs = {}
     for _, key in ipairs(hidden_z) do
@@ -26,6 +25,31 @@ return {
       { "<leader>l", group = "LSP" },
       { "<leader>s", group = "Treesitter" },
       { "<leader>g", group = "Git" },
+
+      -- Fold commands (z)
+      { "z", group = "Folds/View" },
+
+      -- Single fold (under cursor)
+      { "za", desc = "Toggle fold" },
+      { "zA", desc = "Toggle fold (ALL levels)" },
+
+      -- Global fold level (affects whole buffer)
+      { "zm", desc = "↓ foldlevel (close more folds)" },
+      { "zM", desc = "Close ALL folds in buffer" },
+      { "zr", desc = "↑ foldlevel (open more folds)" },
+      { "zR", desc = "Open ALL folds in buffer" },
+
+      -- Navigation
+      { "zj", desc = "Jump to next fold" },
+      { "zk", desc = "Jump to prev fold" },
+
+      -- Toggle folding entirely
+      { "zi", desc = "Toggle folding on/off" },
+      { "zn", desc = "Disable folding" },
+      { "zN", desc = "Enable folding" },
+
+      -- View
+      { "zz", desc = "Center cursor line" },
     })
     vim.keymap.set("n", "<leader>?", function()
       require("which-key").show({ global = false })
