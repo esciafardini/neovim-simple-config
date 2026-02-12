98cf4e9e1f94c5e495b99a25a70fa6003cdb9707
esciafardini <edward.ciafardini@aclaimant.com>
2026-02-12 07:12:24 -0500

    Custom colorscheme
    Make it a little less loud/distracting
    Nice and subtle

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,16 +3,24 @@ return {
   lazy = false,
   priority = 1000,
   opts = {
+    colors = {
+      palette = {
+        carpYellow   = "#b0cfd1",
+        autumnOrange = "#95bec1",
+        autumnYellow = "#bdd8d9",
+      },
+    },
     overrides = function(colors)
       local theme = colors.theme
       return {
-        LineNr = { fg = theme.syn.string, bg = theme.ui.bg_m3 },
-        CursorLineNr = { fg = theme.syn.number, bg = theme.ui.bg_m3, bold = true },
+        LineNr = { fg = theme.syn.type, bg = theme.ui.bg_m3 },
+        CursorLineNr = { fg = theme.syn.string, bg = theme.ui.bg_m3, bold = true },
         SignColumn = { bg = theme.ui.bg_m3 },
       }
     end,
   },
-  config = function()
+  config = function(_, opts)
+    require("kanagawa").setup(opts)
     vim.cmd.colorscheme("kanagawa")
   end,
 }
